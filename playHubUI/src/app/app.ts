c955621fec58f20720f38d4d22f5a2b0af704886
vikasgowda3007 <vikasgowda3007@gmail.com>
2025-09-27 16:43:00 -0700

    Home and header Component

--- playHubUI/src/app/app.ts
+++ playHubUI/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home-component/home-component';
+import { HeaderComponent } from './shared/header-component/header-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
