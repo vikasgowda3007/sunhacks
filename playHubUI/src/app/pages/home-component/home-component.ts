fa117ffbc3d620194f4c10c73f760b7d440cf7c9
dhathri1620 <dhathri16@gmail.com>
2025-09-27 19:12:20 -0700

    Reinitialize repo at correct root

--- playHubUI/src/app/pages/home-component/home-component.ts
+++ playHubUI/src/app/pages/home-component/home-component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home-component.html',
-  styleUrl: './home-component.scss'
+  styleUrls: ['./home-component.scss'], // optional CSS file
 })
 export class HomeComponent {
+  current = 0;
+  slidesArray = [1, 2, 3]; // Number of slides
 
+  nextSlide() {
+    this.current = (this.current + 1) % this.slidesArray.length;
+  }
+
+  prevSlide() {
+    this.current = (this.current - 1 + this.slidesArray.length) % this.slidesArray.length;
+  }
+
+  goToSlide(index: number) {
+    this.current = index;
+  }
 }
