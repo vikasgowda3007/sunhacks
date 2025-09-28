93cc6addc769ba345e0c4bce03d2a063b2423eaf
vikasgowda3007 <vikasgowda3007@gmail.com>
2025-09-28 04:55:44 -0700

    booking form data

--- playHubUI/src/app/pages/home-component/home-component.ts
+++ playHubUI/src/app/pages/home-component/home-component.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-home-component',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatSelectModule, FormsModule, MatRadioModule],
   templateUrl: './home-component.html',
   styleUrls: ['./home-component.scss'], // optional CSS file
 })
@@ -22,5 +27,10 @@ export class HomeComponent {
 
   goToSlide(index: number) {
     this.current = index;
+  }
+
+  findMatch() {
+    // Logic to find a match
+    console.log('Finding a match...');
   }
 }
