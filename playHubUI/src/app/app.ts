d9489d19226c8789f7f78486376e2bf751e1e6fa
vikasgowda3007 <vikasgowda3007@gmail.com>
2025-09-28 02:50:00 -0700

    remove homecomponent and route from app.ts

--- playHubUI/src/app/app.ts
+++ playHubUI/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { HomeComponent } from './pages/home-component/home-component';
 import { HeaderComponent } from './shared/header-component/header-component';
 import { RouterModule } from '@angular/router';
-import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
